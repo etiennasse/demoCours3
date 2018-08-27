87bd3d86521b138ce56b7e94140fed3ccacbf348
B533P-04\533 <etiennasse@hotmail.com>
2018-08-27 17:11:29 -0400

    Bullet pi toute

---
+++ New Unity Project/Assets/Script/FireGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    [SerializeField] GameObject bullet;
+    //[SerializeField] float FireRate = 0.1f;
+    [SerializeField] GameObject bulletEmitter;
+    
+    // Use this for initialization
+   // private float timerlastshot = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //timerlastshot = +Time.deltaTime;
+       // if (timerlastshot > FireRate)
+       // {           
+            var fire1 = Input.GetAxis("Fire1");
+            if (fire1 > 0)
+            {
+                Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+              //  timerlastshot = 0;
+            }
+            
+      //  }
+	}
+}
